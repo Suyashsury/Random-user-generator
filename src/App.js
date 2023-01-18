b24ed5f8683456e3eff78ead14208d4ec3319d27
Suyash Suryawanshi <suyashsuryawanshi2552@gmail.com>
2023-01-18 12:50:44 +0530

    final

--- src/App.js
+++ src/App.js
@@ -3,20 +3,95 @@ import "./App.css";
 import { Button } from "react-bootstrap";
 import Data from "./data.json";
 import { SectionDetails } from "./components/SectionDetails/SectionDetails";
+import Login from "./components/Login/Login";
+import axios from "axios"
+import {useEffect,useState} from "react"
+
+
 function App() {
+  const[userdata,setUserData]=useState(null)
+  const[loading,setLoading]=useState(true)
+  useEffect(()=>{
+    
+    // axios({
+    //   method: 'get',
+    //   url: 'https://randomuser.me/api/'
+      
+    // })
+    //   .then( (response) =>{
+    //     setUserData( response.data.results[0])
+    //     // console.log("useEffect--: once",  response.data.results[0])
+    //   }).catch((error)=>{
+    //     console.log("Error while loading user detail" , error.message)
+    //   })
+    loadDetails()
+      
+    },[])
   const loadDetails = () => {
-    console.log(Data.results);
+    setLoading(true)
+      
+  //   console.log(Data.results);
+    
+  //   const obj={
+  //     ...Data.results[0],
+  //     name:{
+  //       first:"suyash",
+  //       last:"Suryawanshi",
+  //       title:"Mr"
+  //     }
+        
+      
+  //   }
+//     // setUserData(obj)
+axios({
+  method: 'get',
+  url: 'https://randomuser.me/api/'
+  
+})
+  .then( (response) =>{
+    setUserData( response.data.results[0])
+    // console.log("useEffect--: once",  response.data.results[0])
+  }).catch((error)=>{
+    console.log("Error while loading user detail" , error.message)
+  }).finally(()=>{
+    setLoading(false)
+  })
+   
   };
+  
   return (
+    // <h1>ddhjdhj</h1>
+   <div className="main">
+    
+   <div className="login">
+    <img src={userdata.picture.large} />
+   <h6>Login Info</h6>
+   <div className="userinfo">
+<label >Username</label>
+<p>{userdata.login.username}</p>
+</div>
+<div className="password">
+<label >Password</label>
+<p>{userdata.login.password}</p>
+</div>
+   </div>
+    
+    
+   
     <div className="user-generator-page">
       <div className="header-section">
-        <h4>Krishna Bhamare</h4>
+        <h4>{userdata.name.first} {userdata.name.last}</h4>
         <Button variant="dark" onClick={loadDetails}>
           Generate New User
         </Button>
       </div>
-      <SectionDetails detail={Data.results[0]} />
+      <SectionDetails detail={userdata} />
+    
+      
     </div>
+    </div>
+   
+     
   );
 }
 
--- src/components/SectionDetails/SectionDetails.js
+++ src/components/SectionDetails/SectionDetails.js
@@ -13,6 +13,14 @@ export const SectionDetails = (props) => {
           <LineInfo label="Gender" value={props.detail.gender}/>
           <LineInfo label="Age" value={props.detail.dob.age}/>
         </section>
+        <section>
+        <h6>Address</h6>
+          <LineInfo label="Street" value={props.detail.location.street.name}/>
+          <LineInfo label="City" value={props.detail.location.city}/>
+          <LineInfo label="State" value={props.detail.location.state}/>
+          <LineInfo label="Postcode" value={props.detail.location.postcode}/>
+          <LineInfo label="Phone" value={props.detail.phone}/>
+          </section>
       </li>
       
     </ul>
